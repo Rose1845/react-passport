b5efcc158ef585bd1c83e861d7db9da5bcf724ae
Rose Odhiambo <odhiamborose466@gmail.com>
2022-11-13 19:04:20 +0300

    adding postt

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
 
 //import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
-
+//import Home from './pages/Home'
+import Post from './pages/Post'
 function App() {
   
   return (
     <div className="App">
     <Navbar/>
-    <Home/>
+    {/* <Home/> */}
+    <Post/>
      
     </div>
   )
