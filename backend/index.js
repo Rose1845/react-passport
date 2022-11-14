7966fd45278991ed30d87e2ecb9b05cddd4af573
Rose Odhiambo <odhiamborose466@gmail.com>
2022-11-14 18:04:01 +0300

    added the passport ,cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
+const cookieSession = require('cookie-session');
 const express = require('express')
 const app = express()
 
+app.use(cookieSession({
+    name: 'session',
+    keys: ['rose'],
+  
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }))
+
+  app.use(passport.initiliaze())
+  app.use(passport.session())
+
+  app.use(cors({
+    origin:"http://localhost:3000",
+    method:"GET, POSR ,PUT , DELETE",
+    credential:true
+  }))
 app.listen("5000",()=>{
     console.log('server running');
 })
